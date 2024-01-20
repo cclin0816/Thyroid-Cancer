92cfb161b2b4505d1a71c28c0a6d8e2f748ab6a9
cclin0816 <benjamin.cclin@gmail.com>
2024-01-20 16:52:32 +0800

    fix_dataset

--- src/utils.py
+++ src/utils.py
@@ -5,13 +5,16 @@ import sys
 
 
 def load_json(path: Path) -> Any:
-    with path.open("r") as fp:
+    with path.open('r') as fp:
         return json.load(fp)
 
 
 def dump_json(path: Path, obj: Any):
-    with path.open("w") as fp:
+    with path.open('w') as fp:
         json.dump(obj, fp)
+
+
+log_lvl = 4
 
 
 def log(lvl: str, msg: str):
@@ -23,6 +26,9 @@ def log(lvl: str, msg: str):
         print('[WARN] ' + msg, file=sys.stderr)
     elif lvl == 'CRIT' and log_lvl >= 2:
         print('[CRIT] ' + msg, file=sys.stderr)
-    elif lvl == 'ERR' and log_lvl >= 1:
-        print('[ERR] ' + msg, file=sys.stderr)
-    
+    elif lvl == 'ERRO' and log_lvl >= 1:
+        print('[ERRO] ' + msg, file=sys.stderr)
+
+
+Bbox = tuple[int, int, int, int]
+''' type alias of (x_min, y_min, width, height) '''
